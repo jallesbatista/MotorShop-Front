591565452c3581f33a62069cf38beb2591ee5845
Flipsy1 <felipevie@hotmail.com>
2023-05-08 16:31:21 -0400

    feat/no_posters

--- src/components/PosterList.tsx
+++ src/components/PosterList.tsx
@@ -1,4 +1,4 @@
-import { Button, Flex, Grid, useDisclosure } from "@chakra-ui/react";
+import { Button, Flex, Grid, Text, useDisclosure } from "@chakra-ui/react";
 import PosterCard from "./PosterCard";
 import { IPoster } from "@/interfaces/poster.interfaces";
 import PosterCreateEditModal from "./PosterCreateEditModal";
@@ -50,89 +50,104 @@ const PosterList = ({
 
   return (
     <>
-      <Flex
-        wrap={"nowrap"}
-        display={{ base: "flex", lg: "none" }}
-        overflowX={"scroll"}
-        maxW={"100%"}
-        gap={"16px"}
-        px={"25px"}
-        pb={"10px"}
-        justify={posterList.length == 1 ? "center" : "normal"}
-      >
-        {posterList.map((poster, index) => (
-          <Flex direction={"column"} key={index} gap={"12px"}>
-            <PosterCard
-              index={index}
-              poster={poster}
-              showPromoTag={showPromoTag}
-              showStatusTag={showStatusTag}
-              showSeller={showSeller}
-            />
-            {edit && (
-              <Flex color={"grey.1"} gap={"16px"}>
-                <Button
-                  variant={"outline1"}
-                  onClick={() => {
-                    setPoster(poster);
-                    onEditModalOpen();
-                  }}
-                >
-                  Editar
-                </Button>
-                <Button as={Link} href={`/poster/${poster.id}`} variant={"outline1"}>
-                  Ver detalhes
-                </Button>
+      {posterList.length ? (
+        <>
+          <Flex
+            wrap={"nowrap"}
+            display={{ base: "flex", lg: "none" }}
+            overflowX={"scroll"}
+            maxW={"100%"}
+            gap={"16px"}
+            px={"25px"}
+            pb={"10px"}
+            justify={posterList.length == 1 ? "center" : "normal"}
+          >
+            {posterList.map((poster, index) => (
+              <Flex direction={"column"} key={index} gap={"12px"}>
+                <PosterCard
+                  index={index}
+                  poster={poster}
+                  showPromoTag={showPromoTag}
+                  showStatusTag={showStatusTag}
+                  showSeller={showSeller}
+                />
+                {edit && (
+                  <Flex color={"grey.1"} gap={"16px"}>
+                    <Button
+                      variant={"outline1"}
+                      onClick={() => {
+                        setPoster(poster);
+                        onEditModalOpen();
+                      }}
+                    >
+                      Editar
+                    </Button>
+                    <Button as={Link} href={`/poster/${poster.id}`} variant={"outline1"}>
+                      Ver detalhes
+                    </Button>
+                  </Flex>
+                )}
               </Flex>
-            )}
+            ))}
           </Flex>
-        ))}
-      </Flex>
 
-      <Grid
-        gap={{ md: "100px 0px", lg: "100px 20px", xl: "100px 32px" }}
-        templateColumns={{ lg: `repeat(${columns?.lg}, 1fr)`, xl: `repeat(${columns?.xl}, 1fr)` }}
-        gridTemplateRows={"auto"}
-        w={width}
-        alignContent={"center"}
-        maxW={maxWidth}
-        display={{ base: "none", lg: "grid" }}
-        h={"max-content"}
-        justifyItems={"center"}
-        sx={{
-          "@media (min-width: 1320px)": {
-            gridTemplateColumns: `repeat(${maxColumns},1fr)`,
-          },
-        }}
-      >
-        {posterList.map((poster, index) => (
-          <Flex direction={"column"} key={index} gap={"12px"}>
-            <PosterCard
-              index={index}
-              poster={poster}
-              showPromoTag={showPromoTag}
-              showStatusTag={showStatusTag}
-              showSeller={showSeller}
-            />
-            {edit && (
-              <Flex color={"grey.1"} gap={"16px"}>
-                <Button
-                  variant={"outline1"}
-                  onClick={() => {
-                    setPoster(poster);
-                    onEditModalOpen();
-                  }}
-                >
-                  Editar
-                </Button>
-                <Button as={Link} href={`/poster/${poster.id}`} variant={"outline1"}>
-                  Ver detalhes
-                </Button>
+          <Grid
+            gap={{ md: "100px 0px", lg: "100px 20px", xl: "100px 32px" }}
+            templateColumns={{
+              lg: `repeat(${columns?.lg}, 1fr)`,
+              xl: `repeat(${columns?.xl}, 1fr)`,
+            }}
+            gridTemplateRows={"auto"}
+            w={width}
+            alignContent={"center"}
+            maxW={maxWidth}
+            display={{ base: "none", lg: "grid" }}
+            h={"max-content"}
+            justifyItems={"center"}
+            sx={{
+              "@media (min-width: 1320px)": {
+                gridTemplateColumns: `repeat(${maxColumns},1fr)`,
+              },
+            }}
+          >
+            {posterList.map((poster, index) => (
+              <Flex direction={"column"} key={index} gap={"12px"}>
+                <PosterCard
+                  index={index}
+                  poster={poster}
+                  showPromoTag={showPromoTag}
+                  showStatusTag={showStatusTag}
+                  showSeller={showSeller}
+                />
+                {edit && (
+                  <Flex color={"grey.1"} gap={"16px"}>
+                    <Button
+                      variant={"outline1"}
+                      onClick={() => {
+                        setPoster(poster);
+                        onEditModalOpen();
+                      }}
+                    >
+                      Editar
+                    </Button>
+                    <Button as={Link} href={`/poster/${poster.id}`} variant={"outline1"}>
+                      Ver detalhes
+                    </Button>
+                  </Flex>
+                )}
               </Flex>
-            )}
+            ))}
+          </Grid>
+        </>
+      ) : (
+        <>
+          <Flex w={"100%"} gap={"16px"} px={"25px"} pb={"10px"} justify={"center"}>
+            <Text as={"h3"} fontSize={"x-large"} fontWeight={"bold"} mt={"20px"}>
+              Nenhum anúncio encontrado...
+            </Text>
           </Flex>
-        ))}
-      </Grid>
+        </>
+      )}
 
       <PosterCreateEditModal
         poster={poster}
